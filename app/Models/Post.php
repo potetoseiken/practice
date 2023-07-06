ad6dea95c7c3a507585128b4c9cc36a53894c788
potetoseiken <torico222.reiya@gmail.com>
2023-07-06 20:39:27 +0900

    投稿の詳細表示およびペジネーション処理

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    
+    public function getPaginateBylimit(int $limit_count = 1) {
+        // updated_atで降順に並び替えた後、件数を10件に制限した。
+        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+    }
 }
